693c733c39c6b7ebc4aca3d474610218792caf7c
RobertMJr <25443406+RobertMJr@users.noreply.github.com>
2020-04-12 21:10:26 +0300

    Added comments and the complementStrand method

--- main.js
+++ main.js
@@ -13,10 +13,12 @@ const returnRandBase = () => {
     return newStrand
   }
   
+  // Factory function that returns pAequor objects
   function pAequorFactory(specimenNum, dna) {
     return {
       specimenNum,
       dna,
+      // mutate randomly selects a base in the object's dna property and changes it to a different one then returns the object's dna.
       mutate () {
         const currentBaseIndex = Math.floor(Math.random() * 15);
         const currentBase = this.dna[currentBaseIndex];
@@ -25,7 +27,10 @@ const returnRandBase = () => {
           newRandomBase = returnRandBase();
         }
         this.dna[currentBaseIndex] = newRandomBase;
+        return this.dna;
       },
+      /* compareDna takes in another pAequor object, compares how many dna bases are identical in the same locations.
+         prints a message that states the percentage of DNA the two objects have in common */
       compareDna (pAequor) {
         let identicalResults = 0;
         for (let i = 0; i < 15; i++) {
@@ -34,9 +39,11 @@ const returnRandBase = () => {
           } 
         }
         const percentage = identicalResults / this.dna.length * 100;
-        return `Specimen ${this.specimenNum} and specimen ${pAequor.specimenNum} have ${percentage.toFixed 
-  (2)}% in common.`
+        console.log(`Specimen ${this.specimenNum} and specimen ${pAequor.specimenNum} have ${percentage.toFixed 
+  (2)}% in common.`);
       },
+      /* compareDnaV2 takes in another pAequor object, compares how many dna bases are identical in the same locations.
+         returns a message that states the percentage of DNA the two objects have in common */
       compareDnaV2 (pAequor) {
         const result = this.dna.reduce((acc, curr, idx, arr) => {
           if (arr[idx] === pAequor.dna[idx]) {
@@ -50,6 +57,7 @@ const returnRandBase = () => {
         return `Specimen ${this.specimenNum} and specimen ${pAequor.specimenNum} have ${percentage2.toFixed 
           (2)}% in common.`
       },
+      // willLikelySurvive returns true if the object’s .dna array contains at least 60% 'C' or 'G' bases. Otherwise, .willLikelySurvive() returns false.
       willLikelySurvive () {
         let containsBaseCG = 0;
         for (let i = 0; i < 15; i++){
@@ -59,29 +67,51 @@ const returnRandBase = () => {
         }
         const percentageBaseCG = containsBaseCG / 15;
         return percentageBaseCG >= 0.6;
+      },
+      // complementStrand returns the complementary DNA strand. The rules are that 'A's match with 'T's and vice versa. Also, 'C's match with 'G's and vice versa. 
+      complementStrand() {
+        const complementaryDna = [];
+        this.dna.forEach(element => {
+          switch (element) {
+            case "A":
+              complementaryDna.push("T");
+              break
+            case "T":
+              complementaryDna.push("A");
+              break
+            case "C":
+              complementaryDna.push("G");
+              break
+            case "G":
+              complementaryDna.push("C");
+              break
+          }
+        });
+        return complementaryDna;
       }
     }
   };
   
+  // Test out the propreties and methods
   const objekt = pAequorFactory(1, mockUpStrand());
   const objekt2 = pAequorFactory(2, mockUpStrand());
   console.log(objekt.dna);
+  console.log(objekt.complementStrand());
   console.log(objekt.willLikelySurvive());
   console.log(objekt.compareDna(objekt2));
   console.log(objekt.compareDnaV2(objekt2));
   
   const pAequorArr = [];
   let number = 1;
-  let objector = pAequorFactory(number, mockUpStrand());
+
+  // With the factory function set up, create 30 instances of pAequor that can survive in their natural environment. Store these instances in an array.
   while (pAequorArr.length < 30) {
+    let objector = pAequorFactory(number, mockUpStrand());
     if (objector.willLikelySurvive()) {
     pAequorArr.push(objector);
-    number += 1;
-    objector = pAequorFactory(number, mockUpStrand());
     }
-    else {
-      objector = pAequorFactory(number, mockUpStrand());
-    }  
+    number ++;
+
   }
   
   console.log(pAequorArr);
